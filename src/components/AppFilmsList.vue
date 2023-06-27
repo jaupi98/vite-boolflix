c0fc425ea21e46379d2a2c90ff941833997425dc
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-06-27 17:53:06 +0200

    esercizio impostato

---
+++ src/components/AppFilmsList.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+            
+                filmList: []
+            }
+        },
+        mounted() {
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=e5bcdd9db0714a9e718357e96cd962be&query=ritorno+al+fut').then((response) =>{
+                this.filmList = response.data.results;
+            })
+        }           
+    }
+</script>
+<template lang="">
+    <div>
+        <div clas="container">
+            <div class="row">
+                
+                <div v-for="(film,index)  in filmList" :key="index" class="col-2">
+                    <div>{{film.title}}</div>
+                    <div>{{film.original_title}}</div>
+                    <div>{{film.vote_avarage}}</div>
+                    <div>{{film.overview}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+            props:{
+                title:String
+            }
+        }
+ </script>
+<template lang="">
+    <header>
+        <h1 >{{title}}</h1>
+    </header>
+</template>
+<style lang="scss" scoped>
+    h1{
+        font-weight: 700;
+    }
+</style>
